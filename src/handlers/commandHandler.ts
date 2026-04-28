ea22602c8a665677f181cd28478d4fc4764172ad
daannzy9384 <danzihodogta5@gmail.com>
2026-04-28 12:54:23 -0300

    add command /reset xp

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -1,5 +1,5 @@
 import { Client, Collection, REST, Routes } from 'discord.js';
-import { readdirSync } from 'fs';
+import { readdirSync, lstatSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { config } from '../core/config.js';
@@ -13,14 +13,29 @@ export async function loadCommands(client: any) {
 
     const commandsPath = join(__dirname, '..', 'commands');
     
-    const commandFiles = readdirSync(commandsPath).filter(file => 
-        file.endsWith('.ts') || file.endsWith('.js')
-    );
+    const commandFolders = readdirSync(commandsPath);
 
-    for (const file of commandFiles) {
-        const filePath = `file://${join(commandsPath, file)}`;
+    for (const item of commandFolders) {
+        const itemPath = join(commandsPath, item);
+        const stat = lstatSync(itemPath);
+
+    if (stat.isDirectory()) {
+        const commandFiles = readdirSync(itemPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const filePath = `file://${join(itemPath, file)}`;
+            const command = await import(filePath);
+            const cmd = command.default || command;
+
+            if ('data' in cmd && 'execute' in cmd) {
+                client.commands.set(cmd.data.name, cmd);
+                commands.push(cmd.data.toJSON());
+                console.log(`Comando carregado: ${cmd.data.name}`);
+            }
+        }
+    } else if (item.endsWith('.ts') || item.endsWith('.js')) {
+        const filePath = `file://${itemPath}`;
         const command = await import(filePath);
-
         const cmd = command.default || command;
 
         if ('data' in cmd && 'execute' in cmd) {
@@ -29,6 +44,7 @@ export async function loadCommands(client: any) {
             console.log(`Comando carregado: ${cmd.data.name}`);
         }
     }
+}
 
     const rest = new REST({ version: '10' }).setToken(config.token!);
 
@@ -43,3 +59,4 @@ export async function loadCommands(client: any) {
         console.error('Erro ao registrar comandos:', error);
     }
 }
+
